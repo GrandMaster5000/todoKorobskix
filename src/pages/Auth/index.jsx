195c831281a3527610541f463c7d4f33db42f4b1
Tolik22869 <ggamer556@mail.ru>
2021-09-23 15:07:23 +0300

    add styles Auth page

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,34 +1,60 @@
 import React, {useState} from 'react'
-
+import {
+    Typography
+} from 'mdc-react'
 import { actions } from '../../store/store';
+import './index.scss';
 
-const Auth = ({ history }) => {
-    const [login, setLogin] = useState('');
+const Auth = () => {
+    const [email, setEmail] = useState('');
     const [password , setPassword] = useState('');
+    const [error , setError] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+     function handleLogInButtonClick() {
+        if (email && password) {
+            actions.loginUser(email, password)
+                .catch(error => setError(error.message));
+        }
+    }
 
-        actions.loginUser(login, password)
+    function handleRegisterButtonClick() {
+        if (email && password) {
+            actions.registerUser(email, password)
+                .catch(error => setError(error.message));
+        }
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
+        <div className='auth-wrapper'>
+            <h1 className='auth-title'>Korobskix Todo</h1>
+
+            {error &&
+                <Typography>{error}</Typography>
+            }
+
+            <div className='auth-form'>
                 <input
                     type='email'
-                    value={login}
-                    onChange={(e)=> setLogin(e.target.value)}
+                    value={email}
+                    onChange={(e)=> setEmail(e.target.value)}
+                    className='auth-input'
+                    placeholder='Электронная почта'
                     required
                 />
                 <input
                     type='password'
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                    className='auth-input'
+                    placeholder='Пароль'
                     required
                 />
-                <button type='submit'>Войти</button>
-            </form>
+
+                <div className='auth-button-wrapper'>
+                    <button onClick={handleLogInButtonClick} className='auth-button'>ВОЙТИ</button>
+                    <button onClick={handleRegisterButtonClick} className='auth-button'>ЗАРЕГИСТРИРОВАТСЯ</button>
+                </div>
+            </div>
         </div>
     )
 }
